5939d740336919ccde9579b044ced11abb7f3e24
cpragadeesh <cpragadeesh@gmail.com>
2015-03-08 18:45:34 +0530

    Code cleanup.
    Cleaned up older code and used better way of doing things.

--- judge.py
+++ judge.py
@@ -1,54 +1,98 @@
 import subprocess
 import os
-import sys
-import inputGen
+from pageParser import *
+
+def parse_page(contestID, problemID):
+	parser = problemPageParser()
+	source = open_page(contestID, problemID)
+	parser.feed(source)
+	test_cases = parser.return_data()
+	return test_cases
+
+def get_next_test_case(test_cases):
+	test_cases = test_cases[1:]
+	end = -1
+	for i in test_cases:
+		if i == 'Input':
+			end = test_cases.index('Input')
+
+	if end == -1:
+		return test_cases, []
+	else:
+		end = test_cases.index('Input', 1)
+		case = test_cases[:end]
+		test_cases = test_cases[end:]
+		return case, test_cases
+
+def create_input_file(case):
+	with open('input.txt', 'wt') as f:
+		for i in case:
+			if(i == 'Output'):
+				break
+			f.write(i + '\n')
+	return case[:case.index('Output')]
+
+def get_next_output(case):
+	return case[case.index('Output') + 1:]
 
 def build():
-	file_path = sys.argv[1]
-	file_output_name = sys.argv[2]
+
+	file_output_name = sys.argv[1]
+	file_path = sys.argv[2]
+	file_out = file_path[:-4]
+	print 'file_path: ', file_path
+	print 'file_output_name: ', file_output_name
 	print 'Compiling...'
-	compile_result = subprocess.call(['g++', file_path, '-o', file_path[:-4]])
+	compile_result = subprocess.call(['g++', file_path, '-o', file_out])
 	return compile_result
 
-def verdict():
-	contestID = sys.argv[2][:-1]
-	problemID = sys.argv[2][-1]
-	inputGen.generator(contestID, problemID)
-	print 'Running test cases...\n'
-	with open('input.txt', 'rt') as fin, open('output.txt') as fout:
-		for linein, lineout in zip(fin, fout):
-			with open('single_test_case.txt', 'wt') as f:
-				f.write(linein)
+def verify(test_cases):
+	case, test_cases = get_next_test_case(test_cases)
+	input_case = create_input_file(case)
+	expected_output = get_next_output(case)
+	output = subprocess.check_output('./' + contestID + problemID + ' < input.txt', shell = True)
+	print 'Output: ', output
+	output_list = output.split('\n')
+	if output_list[-1] == '':
+		output_list = output_list[:-1]
 
-			os.system('./' + contestID + problemID + ' < single_test_case.txt > single_out.txt')
-			with open('single_out.txt', 'rt') as f:
-				output = f.read()
-				print 'Input:'
-				linein = linein[:-1]
-				print linein
-				print 'Expected Output: '
-				lineout = lineout[:-1]
-				print lineout
-				print 'Your Output:'
-				print output
-				print 'Verdict:',
-				if(lineout == output):
-					print 'CORRECT\n'
-				else:
-					print 'WRONG\n'
-					print 'Incorrect Solution'
-					return 0
-		print "Correct Solution!"
+	print ''
+	print 'Input: '
+	for inp in input_case:
+		print inp
+	print 'Your Output: '
+	for out in output_list:
+		print out
+	print 'Expected output: '
+	for op in expected_output:
+		print op
+	if output_list == expected_output:
+		print 'Test case Passed!'
+		return 0, test_cases, contestID, problemID
+	else:
+		print 'Wrong Answer!'
+		return 1, test_cases, contestID, problemID
+
+def run(test_cases, contestID, problemID):
+
+	if len(test_cases) == 0:
+		print ''
+		print 'All test cases passed successfully!'
+		return 0
+
+	val, test_cases, problemID, contestID = verify(test_cases)
+
+	if val == 0:
+		run(test_cases, contestID, problemID)
+	else:
+		return 1
 
 def cleanup():
-	os.system('rm Input.txt')
-	os.system('rm output.txt')
-	os.system('rm single_out.txt')
-	os.system('rm single_test_case.txt')
+	os.system('rm input.txt')
 
-result = build()
-if result == 0:
-	verdict()
-	cleanup()
-else:
-	print 'Compilation Error.'
+contestID = sys.argv[1][:-1]
+problemID = sys.argv[1][-1]
+build()
+test_cases = parse_page(contestID, problemID)
+run(test_cases, contestID, problemID)
+cleanup()
